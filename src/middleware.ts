4eab7c815749a2f9cf66272ba8df729eb3c7c495
Aaron Ruelas <rnscth@gmail.com>
2025-04-06 00:26:13 -0700

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,22 +3,22 @@ import { routing } from './i18n/routing';
 import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(req: NextRequest) {
-  const res = createMiddleware(routing)(req); // Ejecuta el middleware de next-intl
-
-  // Verifica si la cookie de idioma ya existe
+  const res = createMiddleware(routing)(req);
   const localeCookie = req.cookies.get('NEXT_LOCALE')?.value;
 
-  // Si no existe la cookie de idioma, determinar el idioma predeterminado
+  const browserLang = req.headers.get("accept-language")?.split(",")[0].split("-")[0] || "en";
+
+  // Extraer solo el path (sin el dominio)
+  const pathname = req.nextUrl.pathname;
+
   if (!localeCookie) {
-    const browserLang = req.headers.get("accept-language")?.split(",")[0].split("-")[0] || "en";
     res.cookies.set('NEXT_LOCALE', browserLang, { path: '/', maxAge: 31536000 });
 
-    // Redirige al idioma adecuado si no se encuentra en la URL
-    if (!req.url.includes(`/${browserLang}`)) {
-      return NextResponse.redirect(new URL(`/${browserLang}${req.url}`, req.url));
+    if (!pathname.startsWith(`/${browserLang}`)) {
+      const newUrl = new URL(`/${browserLang}${pathname}`, req.url);
+      return NextResponse.redirect(newUrl);
     }
   } else {
-    // Si la cookie de idioma existe, configura la localización en base a ella
     res.cookies.set('NEXT_LOCALE', localeCookie, { path: '/', maxAge: 31536000 });
   }
 
